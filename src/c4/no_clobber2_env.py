637cac548288ae609368642db43872612bdac1f0
Bartek Muszynski <220bartek@gmail.com>
2025-10-26 15:43:16 -0700

    no-clobber #2 failing to learn

---
+++ src/c4/no_clobber2_env.py
@@ -0,0 +1,110 @@
+import random
+from typing import Any, Tuple, Dict
+import gymnasium as gym
+from gymnasium import spaces
+import numpy as np
+from c4.c4_board import Color
+
+class NoClobber2Env(gym.Env[np.ndarray, int]):
+    metadata = {"render_modes": ["human"]}
+
+    def __init__(self):
+        super().__init__()
+        
+        self.total_move_count = 0
+        self.game_move_count = 0
+        self.illegal_count = 0
+
+        # Obs: (rows, col) -> board plane
+        self.observation_space = spaces.Box(
+            low=-1.0,
+            high=1.0,
+            shape=(9,),
+            dtype=np.float32,
+        )
+        self.action_space = spaces.Discrete(9) # type: ignore
+
+    def reset(
+            self, 
+            *, 
+            seed: int | None=None, 
+            options: dict[str, Any] | None=None
+            ) -> tuple[np.ndarray, dict[str, Any]]:
+        super().reset(seed=seed)
+        self.board = np.zeros((9,), dtype=int)
+        self.game_move_count = 0
+        return self.obs(), {}
+
+    def obs(self) -> np.ndarray:
+        return self.board.astype(np.float32)
+
+    def step(self, hero_action: int):
+        # Make agent move
+        result = self._make_move(hero_action, -10, +1, Color.O)
+        if result[2]:
+            return result
+
+        opponent_action: int = self.suggest_random_legal_move()
+        self.game_move_count += 1
+        self.board[opponent_action] = Color.X.value
+        return self.obs(), 0, False, False, {}
+
+    def suggest_random_legal_move(self) -> int:
+        legal: list[int] = self.legal_moves()
+
+        if len(legal) == 0:
+            raise Exception("No legal moves")
+        
+        return random.choice(legal)
+
+    def legal_moves(self) -> list[int]:
+        legal: list[int] = []
+
+        for ii in range(9):
+            if self.board[ii] == Color.NONE.value:
+                legal.append(ii)
+
+        return legal
+
+    def _make_move(
+            self, action: int, 
+            illegal_penalty: int,
+            win_reward: int,
+            color: Color
+            ) -> Tuple[np.ndarray, float, bool, bool, Dict[str,str]]:
+        
+        self.total_move_count += 1
+        self.game_move_count += 1
+        
+        if self.total_move_count % 100 == 0:
+            print(f"Illegal: {self.illegal_count} / {self.total_move_count} => {self.illegal_count / self.total_move_count:.4f}")
+
+        # Check for illegal moves
+        if action not in self.legal_moves():
+            self.illegal_count += 1
+            return self.obs(), illegal_penalty, True, False, {f"illegal_move_by_{color}": "True"}
+
+        self.board[action] = color.value
+
+        if self.o_wins():
+            return self.obs(), win_reward, True, False, {"winner": str(color)}
+
+        return self.obs(), 0.0, False, False, {}  # No reward or punishment
+
+    def o_wins(self) -> bool:
+        return self.game_move_count == 9
+
+    def render(self):
+        print(self.to_string())
+
+    def to_string(self) -> str:
+        symbol_map = {
+            0: ".",
+            1: "O",
+            -1: "X"
+        }
+
+        return " ".join(symbol_map[val] for val in self.board)
+
+    def close(self):
+        pass
---
+++ src/c4/no_clobber2_reinforcement_learning.py
@@ -0,0 +1,91 @@
+import os
+from stable_baselines3 import PPO, DQN, A2C
+from stable_baselines3.common.base_class import BaseAlgorithm
+
+from c4.no_clobber2_env import NoClobber2Env, Color
+
+model_path_1 = "nc_player1_dqn.zip"
+
+if os.path.exists(model_path_1):
+    print("Loading existing models...")
+    player1 = DQN.load(model_path_1)
+else:
+    dummy_env: NoClobber2Env = NoClobber2Env()
+    common_params = dict(
+        policy="MlpPolicy",
+        env=dummy_env,
+        learning_rate=0.3,
+        buffer_size=2000,
+        learning_starts=500,
+        batch_size=64,
+        tau=1.0,
+        gamma=0.9,
+        train_freq=1,
+        target_update_interval=500,
+        exploration_fraction=0.4,
+        exploration_final_eps=0.5,
+        verbose=0,
+    )
+
+    # Two agents using the same settings
+    player1 = DQN(**common_params)
+    # player2 = DQN(**common_params)
+
+    LEARNING_ITERATIONS: int = 1
+    TIME_STEPS: int = 20000
+
+    for iteration in range(LEARNING_ITERATIONS):
+        # Learn for Player 1 (O's)
+        print(f"Iteration {iteration} for Player O")
+        env1: NoClobber2Env = NoClobber2Env()
+        player1.set_env(env1)
+        player1.learn(TIME_STEPS)
+
+        # Learn for Player 2 (X's)
+        # print(f"Iteration {iteration} for Player X")
+        # env2: TttEnv = TttEnv(player1, Color.X)
+        # player2.set_env(env2)
+        # player2.learn(TIME_STEPS)
+
+    player1.save("nc_player1_dqn")
+    # player2.save("nc_player2_dqn")
+
+# Execute a sample game
+def execute_game(player1: BaseAlgorithm, deterministic: bool):
+    board: NoClobber2Env = NoClobber2Env()
+    board.reset()
+
+    while True:
+        # Hero Move
+        move_arr, _ = player1.predict(board.obs(), deterministic=deterministic)
+        move: int = int(move_arr)
+
+        if move not in board.legal_moves():
+            print(f"Terminating due to invalid move by O: {move}")
+            break
+
+        print(f"About to make move {move} for O")
+        board.board[move] = Color.O.value
+        board.render()
+
+        if board.o_wins():
+            print(f"O Won!!!")
+            break
+
+        # Opponent Move
+        move = board.suggest_random_legal_move()
+        print(f"About to make move {move} for X")
+        board.board[move] = Color.X.value
+        board.render()
+
+while True:
+    print()
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>> NEW GAME <<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+    execute_game(player1, True)
+
+    command: str = input("ENTER Q to quit: ")
+    if command == 'q' or command == 'Q':
+        break
+
+
+
